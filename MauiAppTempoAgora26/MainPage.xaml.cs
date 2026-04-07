8866a1c800f65a1fce43abb8bfee6ce2269fd2d1
Vitor_Iannaccone <vitor.ianacoli@gmail.com>
2026-04-07 11:53:41 -0300

    Criação e atualização do API e frontend.

--- MauiAppTempoAgora26/MainPage.xaml.cs
+++ MauiAppTempoAgora26/MainPage.xaml.cs
@@ -1,24 +1,66 @@
-﻿namespace MauiAppTempoAgora26
+﻿using MauiAppTempoAgora26.Models;
+using MauiAppTempoAgora26.Services;
+
+namespace MauiAppTempoAgora26
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Localização:\n" +
+                                         $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n\n" +
+                                         $"Clima:\n" +
+                                         $"{t.main}  - {t.description} \n\n" +
+                                         $"Temperatura:\n" +
+                                         $"Temp Máx: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n\n" +
+                                         $"Visibilidade:\n" +
+                                         $"{t.visibility} metros \n\n" +
+                                         $"Sol:\n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n";
+                                         
+
+                        lbl_res.Text = dados_previsao;
+
+                          
+                    }
+                    else
+                    {
+
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
         }
+
+        
     }
 }
+
---
+++ MauiAppTempoAgora26/Resources/DataService.cs
@@ -0,0 +1,62 @@
+﻿using MauiAppTempoAgora26.Models;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace MauiAppTempoAgora26.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&lang=pt_br&appid={chave}";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage resp = await client.GetAsync(url);
+
+                    if (resp.StatusCode == HttpStatusCode.NotFound)
+                        throw new Exception("Cidade não encontrada.");
+
+                    if (!resp.IsSuccessStatusCode) throw new Exception("Erro ao buscar dados da previsão.");
+
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = DateTimeOffset
+                    .FromUnixTimeSeconds((long)rascunho["sys"]["sunrise"]).LocalDateTime;
+                    DateTime sunset = DateTimeOffset
+                        .FromUnixTimeSeconds((long)rascunho["sys"]["sunset"]).LocalDateTime;
+
+                    t = new Tempo
+                    {
+                        lat = (double)rascunho["coord"]["lat"],
+                        lon = (double)rascunho["coord"]["lon"],
+                        description = (string)rascunho["weather"][0]["description"],
+                        main = (string)rascunho["weather"][0]["main"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        visibility = (int)rascunho["visibility"],
+                        sunrise = sunrise.ToString("HH:mm"),
+                        sunset = sunset.ToString("HH:mm"),
+                    }; // Fecha obj do Tempo.
+                } // fecha laço using
+
+            }
+            catch (HttpRequestException)
+            {
+                throw new Exception("Sem conexão com a internet.");
+
+            }
+
+            return t;
+        }
+    }
+}
